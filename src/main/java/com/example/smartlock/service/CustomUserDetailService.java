ab6cf05d4a57df87e586ce7f545d8002fb371ebd
Xrystiliano <egor1gorbatyuk@gmail.com>
2025-11-20 23:09:35 +0200

    Added UserDetailService and JwtAuthFilter for Spring Security

---
+++ src/main/java/com/example/smartlock/service/CustomUserDetailService.java
@@ -0,0 +1,38 @@
+package com.example.smartlock.service;
+
+import com.example.smartlock.entity.User;
+import com.example.smartlock.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        List<SimpleGrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_USER"));
+
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPasswordHash(),
+                authorities
+        );
+    }
+}
