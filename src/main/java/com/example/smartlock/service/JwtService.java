d712c0400a3b68a635de4ac691b744181a7f0217
Xrystiliano <egor1gorbatyuk@gmail.com>
2025-11-20 23:09:54 +0200

    small fix in JwtService

--- src/main/java/com/example/smartlock/service/JwtService.java
+++ src/main/java/com/example/smartlock/service/JwtService.java
@@ -44,15 +44,15 @@ public class JwtService {
         return extractExpiration(token).before(new Date());
     }
 
-    private String extractUsername(String token) {
+    public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
 
-    private Date extractExpiration(String token) {
+    public Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
 
-    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
