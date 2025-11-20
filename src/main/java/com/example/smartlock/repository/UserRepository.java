704c867fb89e3d68656e95e6f3ce11494cb5425a
Xrystiliano <egor1gorbatyuk@gmail.com>
2025-11-20 23:09:04 +0200

    Added findUserByEmail in UserRepository.java

--- src/main/java/com/example/smartlock/repository/UserRepository.java
+++ src/main/java/com/example/smartlock/repository/UserRepository.java
@@ -5,8 +5,11 @@ import jakarta.persistence.Entity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    public Optional<User> findByEmail(String email);
 }
