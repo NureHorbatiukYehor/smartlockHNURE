a82b3f4fb7813ceb7d3600fdd3cc63bdabf1a637
Xrystiliano <egor1gorbatyuk@gmail.com>
2025-11-26 10:18:50 +0200

    Added getters/setter to ErrorResponse, changed version of springdoc in pom.xml

--- src/main/java/com/example/smartlock/exceptions/ErrorResponse.java
+++ src/main/java/com/example/smartlock/exceptions/ErrorResponse.java
@@ -12,4 +12,28 @@ public class ErrorResponse {
         this.errorCode = errorCode;
         this.timestamp = OffsetDateTime.now();
     }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public OffsetDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(OffsetDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
 }
